4bcef848620e2a9531fc707c237d340535268af6
Eduardo Karpfenstein <edukarp10@gmail.com>
2024-09-23 15:36:45 -0300

    :wrench: fix: ensure semantic tags are used on footer and header

--- quick_table_app/app/_components/header.tsx
+++ quick_table_app/app/_components/header.tsx
@@ -11,25 +11,27 @@ import Link from "next/link";
 
 const Header = () => {
     return ( 
-        <Card> 
-            <CardContent className="p-5 justify-between items-center flex flex-row">
-                <Link href="/">
-                    <Image src="/logo_dark.png" alt="logoPlaceholder" width={120} height={22} />
-                </Link>
-                <Sheet>
-                    <SheetTrigger asChild>
-                        <Button variant="outline" size="icon">
-                            <MenuIcon size={18} />
-                        </Button>
-                    </SheetTrigger>
+        <header>
+            <Card> 
+                <CardContent className="p-5 justify-between items-center flex flex-row">
+                    <Link href="/">
+                        <Image src="/logo_dark.png" alt="logoPlaceholder" width={120} height={22} />
+                    </Link>
+                    <Sheet>
+                        <SheetTrigger asChild>
+                            <Button variant="outline" size="icon">
+                                <MenuIcon size={18} />
+                            </Button>
+                        </SheetTrigger>
 
-                    <SheetContent className="p-0">
-                        <SideMenu/>
-                        
-                    </SheetContent>
-                </Sheet>
-            </CardContent>
-        </Card>
+                        <SheetContent className="p-0">
+                            <SideMenu/>
+                            
+                        </SheetContent>
+                    </Sheet>
+                </CardContent>
+            </Card>
+        </header>
      );
 }
  
